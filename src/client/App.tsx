72c867212e1b9ab5cca2ee25a41d2e080be17e97
Facundo Javier Gelatti <javiergelatti@gmail.com>
2020-03-31 20:40:03 -0300

    Extraer CursoRemoto de App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import VistaCurso from './VistaCurso';
-import { crearEstudiante, Persona, Curso, entra, bajaLaMano, levantaLaMano, crearDocente, IdPersona, Evento, sale } from '../model/curso';
 import FormularioLogin from './FormularioLogin';
-import { deserializarCurso, serializarEvento } from '../model/jsonCodecs';
+import { CursoRemoto } from './CursoRemoto';
 
 type Props = {
   docente: boolean,
@@ -12,89 +11,6 @@ type State = {
   cursoRemoto: CursoRemoto | null,
 }
 
-type ObservadorCurso = (cursoRemoto: CursoRemoto) => void;
-
-class CursoRemoto {
-  idUsuarioActual: IdPersona;
-  curso: Curso;
-  enviarEvento: (evento: Evento) => Promise<Curso>;
-
-  static conectarseComo(nombre: string, esDocente: boolean, onChange: ObservadorCurso) {
-    return new Promise(resolve => {
-      const usuarioActual = esDocente ? crearDocente(nombre) : crearEstudiante(nombre);
-
-      const requestsAResolver: ((curso: Curso) => void)[] = [];
-      const websocket = new WebSocket('ws://localhost:8080');
-      let cursoRemoto: CursoRemoto | null = null;
-
-      websocket.onopen = (evt) => {
-        console.log('open', evt);
-        window.addEventListener("beforeunload", () => {
-          enviarEvento(sale(usuarioActual));
-          websocket.close()
-        }, false);
-        enviarEvento(entra(usuarioActual));
-      };
-      websocket.onmessage = (msg) => {
-        console.log('message', msg);
-        const curso = deserializarCurso(msg.data);
-        console.log(curso);
-
-        // Qué pasa si el usuario actual no está en el curso?
-        // curso.personas.find(p => p.id === idUsuarioActual) || null;
-        if (!cursoRemoto) {
-          cursoRemoto = new CursoRemoto(usuarioActual.id, curso, enviarEvento);
-          resolve(cursoRemoto);
-        } else {
-          cursoRemoto.curso = curso;
-        }
-        onChange(cursoRemoto);
-
-        requestsAResolver.forEach(resolve => resolve(curso));
-      };
-      websocket.onclose = (evt) => {
-        console.log('close', evt);
-        // Reconectar?
-      };
-      websocket.onerror = (evt) => console.log('error', evt);
-
-      async function enviarEvento(evento: Evento) {
-        websocket.send(serializarEvento(evento));
-
-        return new Promise<Curso>(resolve => {
-          requestsAResolver.push(resolve);
-        });
-      }
-    });
-  }
-
-  constructor(idUsuarioActual: IdPersona, curso: Curso, enviarEvento: (evento: Evento) => Promise<Curso>) {
-    this.curso = curso;
-    this.idUsuarioActual = idUsuarioActual;
-    this.enviarEvento = enviarEvento;
-  }
-
-  get usuarioActual() {
-    const usuarioActual = this.curso.personas.find(p => p.id === this.idUsuarioActual);
-
-    if (!usuarioActual) throw new Error("El usuario actual no está en el curso");
-
-    return usuarioActual;
-  }
-
-  levantarLaMano() {
-    return this.enviarEvento(levantaLaMano(this.usuarioActual));
-  }
-
-  bajarLaMano() {
-    return this.enviarEvento(bajaLaMano(this.usuarioActual));
-  }
-
-  bajarleLaManoA(unaPersona: Persona) {
-    return this.enviarEvento(bajaLaMano(unaPersona));
-  }
-}
-
 export default class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
@@ -102,7 +18,7 @@ export default class App extends Component<Props, State> {
   }
 
   render() {
-    const cursoRemoto = this.cursoRemoto;
+    const cursoRemoto = this.state.cursoRemoto;
     return (
       <div className="App">
         {
@@ -110,9 +26,9 @@ export default class App extends Component<Props, State> {
             <VistaCurso
               usuarioActual={cursoRemoto.usuarioActual}
               curso={cursoRemoto.curso}
-              onBajarMano={() => this.bajarLaMano()}
-              onLevantarMano={() => this.levantarLaMano()}
-              onBajarleLaManoA={persona => this.bajarleLaManoA(persona)}
+              onBajarMano={() => cursoRemoto.bajarLaMano()}
+              onLevantarMano={() => cursoRemoto.levantarLaMano()}
+              onBajarleLaManoA={persona => cursoRemoto.bajarleLaManoA(persona)}
             />
             :
             <FormularioLogin onLogin={nombre => this.entrarComo(nombre)} />
@@ -123,21 +39,5 @@ export default class App extends Component<Props, State> {
 
   async entrarComo(nombre: string) {
     return CursoRemoto.conectarseComo(nombre, this.props.docente, cursoRemoto => this.setState({ cursoRemoto }));
-  }
-
-  async bajarLaMano() {
-    return this.cursoRemoto?.bajarLaMano();
-  }
-
-  async levantarLaMano() {
-    return this.cursoRemoto?.levantarLaMano();
-  }
-
-  async bajarleLaManoA(unaPersona: Persona) {
-    return this.cursoRemoto?.bajarleLaManoA(unaPersona);
-  }
-
-  get cursoRemoto() {
-    return this.state.cursoRemoto;
   }
 }
---
+++ src/client/CursoRemoto.ts
@@ -0,0 +1,85 @@
+import { crearEstudiante, Persona, Curso, entra, bajaLaMano, levantaLaMano, crearDocente, IdPersona, Evento, sale } from '../model/curso';
+import { deserializarCurso, serializarEvento } from '../model/jsonCodecs';
+
+type ObservadorCurso = (cursoRemoto: CursoRemoto) => void;
+
+export class CursoRemoto {
+  idUsuarioActual: IdPersona;
+  curso: Curso;
+  enviarEvento: (evento: Evento) => Promise<Curso>;
+
+  static conectarseComo(nombre: string, esDocente: boolean, onChange: ObservadorCurso) {
+    return new Promise(resolve => {
+      const usuarioActual = esDocente ? crearDocente(nombre) : crearEstudiante(nombre);
+
+      const requestsAResolver: ((curso: Curso) => void)[] = [];
+      const websocket = new WebSocket('ws://localhost:8080/');
+      let cursoRemoto: CursoRemoto | null = null;
+
+      websocket.onopen = (evt) => {
+        console.log('open', evt);
+        window.addEventListener("beforeunload", () => {
+          enviarEvento(sale(usuarioActual));
+          websocket.close()
+        }, false);
+        enviarEvento(entra(usuarioActual));
+      };
+      websocket.onmessage = (msg) => {
+        console.log('message', msg);
+        const curso = deserializarCurso(msg.data);
+        console.log(curso);
+
+        // Qué pasa si el usuario actual no está en el curso?
+        // curso.personas.find(p => p.id === idUsuarioActual) || null;
+        if (!cursoRemoto) {
+          cursoRemoto = new CursoRemoto(usuarioActual.id, curso, enviarEvento);
+          resolve(cursoRemoto);
+        } else {
+          cursoRemoto.curso = curso;
+        }
+        onChange(cursoRemoto);
+
+        requestsAResolver.forEach(resolve => resolve(curso));
+      };
+      websocket.onclose = (evt) => {
+        console.log('close', evt);
+        // Reconectar?
+      };
+      websocket.onerror = (evt) => console.log('error', evt);
+
+      async function enviarEvento(evento: Evento) {
+        websocket.send(serializarEvento(evento));
+
+        return new Promise<Curso>(resolve => {
+          requestsAResolver.push(resolve);
+        });
+      }
+    });
+  }
+
+  constructor(idUsuarioActual: IdPersona, curso: Curso, enviarEvento: (evento: Evento) => Promise<Curso>) {
+    this.curso = curso;
+    this.idUsuarioActual = idUsuarioActual;
+    this.enviarEvento = enviarEvento;
+  }
+
+  get usuarioActual() {
+    const usuarioActual = this.curso.personas.find(p => p.id === this.idUsuarioActual);
+
+    if (!usuarioActual) throw new Error("El usuario actual no está en el curso");
+
+    return usuarioActual;
+  }
+
+  levantarLaMano() {
+    return this.enviarEvento(levantaLaMano(this.usuarioActual));
+  }
+
+  bajarLaMano() {
+    return this.enviarEvento(bajaLaMano(this.usuarioActual));
+  }
+
+  bajarleLaManoA(unaPersona: Persona) {
+    return this.enviarEvento(bajaLaMano(unaPersona));
+  }
+}
